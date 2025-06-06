293a04a2d051a10d8fd9c43fba42c431826b631f
alejandro2Lopez <ale1jandro.lopez@gmail.com>
2025-06-06 11:13:01 -0600

    ca-stat

--- src/components/Api_Connect.jsx
+++ src/components/Api_Connect.jsx
@@ -12,7 +12,7 @@ export const getFetch = async (sb, path) => {
     });
     if (!res.ok) {
       showErrorAlert("Error", "Ocurrió un error inesperado. Intenta nuevamente.", "error");
-     console.error('Error del servidor:', response);
+     console.error('Error del servidor:', res);
       return false;
     }
     else {
