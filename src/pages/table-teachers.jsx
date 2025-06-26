7ec254464b603a25fda614763e80c1834bc89030
alejandro2Lopez <ale1jandro.lopez@gmail.com>
2025-06-26 15:05:31 -0600

    small change

--- src/pages/table-teachers.jsx
+++ src/pages/table-teachers.jsx
@@ -222,7 +222,7 @@ const Table_teacher = () => {
                                 />
 
                             </button>
-                            {!teacher.hasGroups && (<button
+                            {teacher.hasGroups && (<button
                                 type="button"
                                 style={{ background: "transparent", borderColor: "transparent" }}
                                 onClick={handleDeleteTeacher}
