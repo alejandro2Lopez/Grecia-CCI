5e7a56a165fb740ab0d15e46df582cdd11fc7a5f
alejandro2Lopez <ale1jandro.lopez@gmail.com>
2025-07-09 09:34:56 -0600

    few-changes-about - security

--- src/routes/ManagementRouter.jsx
+++ src/routes/ManagementRouter.jsx
@@ -1,34 +1,36 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import AppRouter from "./AppRouter";
-import Login from "../pages/login";
 import PublicRouter from "./PublicRouter";
 import PrivateRouter from "./PrivateRouter";
+import { Loading } from "../components/Component_loading";
+// Carga dinámica de Login
+const Login = lazy(() => import("../pages/login"));
 
 const ManagementRouter = () => {
   return (
     <Router>
-      <Routes>
-        <Route
-          path="/login"
-          element={
-            <PublicRouter>
-              <Login />
-            </PublicRouter>
-          }
-        />
-     
-
-        <Route
-          path="*"
-          element={
-            <PrivateRouter>
-              <AppRouter />
-            </PrivateRouter>
-          }
-        />
-      </Routes>
+      <Suspense fallback={<Loading/>}>
+        <Routes>
+          <Route
+            path="/login"
+            element={
+              <PublicRouter>
+                <Login />
+              </PublicRouter>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <PrivateRouter>
+                <AppRouter />
+              </PrivateRouter>
+            }
+          />
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
